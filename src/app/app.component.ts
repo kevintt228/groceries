855fb3db9e9a3d1dbc8fb88ee4e43a1496ddab5a
Tang <tt228@waikato.ac.nz>
2020-08-19 14:55:32 +1200

    move images directory to src directory

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./login/login.component.html",
+  styleUrls: ["./login/login.component.scss"]
+})
+export class AppComponent {}
